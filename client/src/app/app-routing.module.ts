9c76473bfae43ee050fd7a1cbd7a272405cede29
blaise.fotso <blaise.fotso@2tcorp.org>
2024-04-09 16:03:50 +0100

    Add pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    runGuardsAndResolvers: 'always',
+    component: AppComponent
+  },
+  {
+    path: 'services',
+    runGuardsAndResolvers: 'always',
+    component: AppComponent
+  },
+  {
+    path: 'products',
+    runGuardsAndResolvers: 'always',
+    component: AppComponent
+  },
+  {
+    path: 'login',
+    runGuardsAndResolvers: 'always',
+    component: AppComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
