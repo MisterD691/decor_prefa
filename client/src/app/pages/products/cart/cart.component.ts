22bee9df73552ba5f1ce2c1f9aa612eeb00c57a0
blaise.fotso <blaise.fotso@2tcorp.org>
2024-04-14 09:42:55 +0100

    Add some changes

--- client/src/app/pages/products/cart/cart.component.ts
+++ client/src/app/pages/products/cart/cart.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  public qty: number = 0;
+
+  addQty(): void {
+    this.qty += 1;
+  }
+
+  reduceQty(): void {
+    this.qty -= 1;
+  }
+
+  removeItem(): void {
+    alert("Btn clicked");
+  }
 
 }
