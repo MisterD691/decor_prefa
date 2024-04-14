d6118882e3e0a55e81736792883802435361687a
blaise.fotso <blaise.fotso@2tcorp.org>
2024-04-14 11:00:39 +0100

    Add some changes

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  public title: string = "Decor Prefa";
+  public title: string = "Bienvenue sur notre plateforme Decor Prefa";
   public content: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla commodo, ante a sagittis euismod, felis nisi imperdiet urna, sed tincidunt dolor neque non turpis. Nullam efficit...";
 
   ngOnInit(): void {
