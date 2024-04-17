19e780ef3a9d8d4ad031b5f9f6ed9fc47724e84f
blaise.fotso <blaise.fotso@2tcorp.org>
2024-04-17 14:25:00 +0100

    Filter funcs per role

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -9,8 +12,23 @@ export class HeaderComponent implements OnInit {
   @Input() public isBig: boolean = true;
   @Input() public title: string = "";
   @Input() public content: string = "";
+  public loading: boolean = false;
+
+  protected user: any;
+  
+  constructor(
+    protected auth: AuthService,
+    protected userService: UserService,
+    protected router: Router,
+  ) {
+    this.user = auth.getUserObject();
+  }
 
   ngOnInit(): void {
     //
+  }
+
+  public logout(): void {
+    this.userService.logout();
   }
 }
--- client/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.wethehivers.com',
-  webUrl: 'https://wethehivers.com',
-};
