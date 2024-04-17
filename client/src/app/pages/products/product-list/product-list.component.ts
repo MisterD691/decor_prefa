a107d2755521943b9aaf9fc077905f2ca1896b2d
blaise.fotso <blaise.fotso@2tcorp.org>
2024-04-17 10:10:21 +0100

    Filter funcs per role

--- client/src/app/pages/products/product-list/product-list.component.ts
+++ client/src/app/pages/products/product-list/product-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/auth/auth.service';
 import { Category } from 'src/app/services/category/category';
 import { CategoryService } from 'src/app/services/category/category.service';
 import { Product } from 'src/app/services/product/product';
 import { ProductService } from 'src/app/services/product/product.service';
+import { User } from 'src/app/services/user/user';
 
 @Component({
   selector: 'app-product-list',
@@ -13,12 +15,15 @@ export class ProductListComponent implements OnInit {
   public products: Product[] = [];
   public categories: Category[] = [];
   public selectedCatId: string = "";
+  protected role: string = "";
 
   constructor(
     private productService: ProductService,
-    private categoryService: CategoryService
-  ) {}
-
+    private categoryService: CategoryService,
+    protected auth: AuthService,
+  ) {
+    this.role = auth.getRole();
+  }
 
   ngOnInit(): void {
     this.getProducts();
